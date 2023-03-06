4070f8abd750285ece678d0ca0b3402544e99477
Matvei Bykadorov <2kydepsta2@gmail.com>
2023-03-06 22:39:00 +0300

    Initial Commit

---
+++ AppSwiftUI/AppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppSwiftUIApp.swift
+//  AppSwiftUI
+//
+//  Created by Matvei Bykadorov on 06.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct AppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
